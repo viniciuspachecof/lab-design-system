bcb7894476063626d77ce47277ca5b1ebcd5c906
Vinicius Pacheco <vinicius@pachecoof@hotmail.com>
2022-10-24 20:26:17 -0300

    alteração (main.js)

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -22,7 +22,7 @@ module.exports = {
   ],
   viteFinal: (config, { configType }) => {
     if (configType === 'PRODUCTION') {
-      config.base = '/lab-ds/'
+      config.base = '/lab-design-system/'
     }
 
     return config
